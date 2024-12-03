ef79b577e89c5b060bb5c621c178178cdc83fe1e
NoemiMolina <molinanoemi5@gmail.com>
2024-12-03 17:05:06 +0100

    modifications, css improved, presentation text added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,49 +43,8 @@ const Header = () => {
                     <div className="menu-item" onClick={() => handleNavigation('/Lukaswork')}>
                         Lukas <IoIosArrowForward style={{ fontSize: '14px' }} />
                     </div>
-                    <div
-                        className="menu-item"
-                        onMouseEnter={() => setIsExhibitionTabOpen(true)}
-                        onMouseLeave={() => setIsExhibitionTabOpen(false)}
-                    >
-                        Exhibitions and previous events<IoIosArrowForward style={{ fontSize: '14px' }} />
-                        {isExhibitionsTabOpen && (
-                            <div className="sub-menu-list">
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/nfccRoma')}>
-                                    <span style={{ fontWeight: 'normal' }}>07/2024 :</span> <span style={{ fontWeight: 'bold' }}>NFCC ROME - DIGITAL FESTIVAL (ROMA)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/nfcLisbon')}>
-                                    <span style={{ fontWeight: 'normal' }}>06/2024 :</span> <span style={{ fontWeight: 'bold' }}>NFC - DIGITAL FESTIVAL (LISBON)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/shinSekaInox')}>
-                                    <span style={{ fontWeight: 'normal' }}>05/2024 :</span> <span style={{ fontWeight: 'bold' }}>SHIN SEKAI - NOX GALLERY (TOKYO)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/shinSekaiCourtyardHiro')}>
-                                    <span style={{ fontWeight: 'normal' }}>05/2024 :</span> <span style={{ fontWeight: 'bold' }}>SHIN SEKAI - COURTYARD HIRO (TOKYO)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/shinSekaiSomsocGallery')}>
-                                    <span style={{ fontWeight: 'normal' }}>05/2024 :</span> <span style={{ fontWeight: 'bold' }}>SHIN SEKAI - SOMSOC GALLERY (TOKYO)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/shinSekaiUltraSuperNew')}>
-                                    <span style={{ fontWeight: 'normal' }}>05/2024 :</span> <span style={{ fontWeight: 'bold' }}>SHIN SEKAI - ULTRA SUPER NEW (TOKYO)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/echoUncommonGallery')}>
-                                    <span style={{ fontWeight: 'normal' }}>04/2024 :</span> <span style={{ fontWeight: 'bold' }}>ECHO - UNCOMMON GALLERY (SEOUL)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/chromaCorGallery')}>
-                                    <span style={{ fontWeight: 'normal' }}>03/2024 :</span> <span style={{ fontWeight: 'bold' }}>CHROMA - COR GALLERY (PARIS)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/EspritDeCorps')}>
-                                    <span style={{ fontWeight: 'normal' }}>02/2024 :</span> <span style={{ fontWeight: 'bold' }}>ESPRIT DE CORPS - NFT FACTORY (PARIS)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/charitableSale')}>
-                                    <span style={{ fontWeight: 'normal' }}>10/2023 :</span> <span style={{ fontWeight: 'bold' }}>CHARITABLE SALE - IHAM (PARIS)</span>
-                                </div>
-                                <div className="sub-menu-item" onClick={() => handleNavigation('/somaverse')}>
-                                    <span style={{ fontWeight: 'normal' }}>09/2023 :</span> <span style={{ fontWeight: 'bold' }}>SOMAVERSE - NFT FACTORY (PARIS)</span>
-                                </div>
-                            </div>
-                        )}
+                    <div className="menu-item" onClick={() => handleNavigation('/Exhibitions')}>
+                       Events & Exhibitions<IoIosArrowForward style={{ fontSize: '14px' }} />
                     </div>
                 </div>
             )}
