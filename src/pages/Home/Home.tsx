fc9cdc2b35c04288556dc413b4932ca7270f5515
NoemiMolina <molinanoemi5@gmail.com>
2024-12-05 11:56:23 +0100

    polygon tab started

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,44 +2,136 @@ import React from 'react';
 import './Home.css';
 import somalogo from '../../assets/somalogo/logo.jpg';
 import presentation from '../../assets/images/presentation.png';
-import evaImgLouisDazy from '../../assets/images/LouisDazyImgs/Eva.jpg';
 import { useNavigate } from 'react-router-dom';
+
+interface CustomCSSProperties extends React.CSSProperties {
+    '--image-url'?: string;
+}
 
 const Home = () => {
     const navigate = useNavigate();
 
-    const handleLouisDazyImgClick = () => {
-        navigate('/LouisDazy');
-    };
+    const presentationImages = [
+        {
+            src: require('../../assets/images/LouisDazyImgs/Eva.jpg'),
+            title: 'Eva',
+            artist: 'Louis Dazy',
+            route: '/LouisDazy',
+        },
+        {
+            src: require('../../assets/images/OelhanImgs/Sailor.gif'),
+            title: 'Sailor',
+            artist: 'Oelhan',
+            route: '/Oelhantv',
+            
+        },
+        {
+            src: require('../../assets/images/PolygonImgs/Zaora.jpg'),
+            title: 'Zaora',
+            artist: 'Polygon1993',
+            route: '/Polygon',
+        }
+     
+    ];
 
     return (
-
         <div className="container">
             <img src={somalogo} alt="Logo" className="logo" />
             <div className="presentation-container">
-                <img src={presentation} alt="Presentation pic" className="presentationPic" />
+                <img
+                    src={presentation}
+                    alt="Presentation pic"
+                    className="presentationPic"
+                />
                 <div className="text-columns">
                     <div className="text-column">
-                        <p>SOMA Collective stands as a testament to the boundless power of art to transcend borders, unite diverse cultures, and connect people through a shared creative experience. Our collective mission is to bridge the realms of the physical and the digital, weaving together a tapestry of artistic expression that blurs traditional boundaries. Our passion lies in creating exhibitions worldwide that blend physical and digital artworks, crafting truly unique events that resonate across the globe.</p>
-                        <p>In a world where the boundaries between the tangible and the virtual continue to blur, SOMA Collective embraces this transformation as an opportunity for exploration. We believe that the fusion of the physical and the digital is not a mere convergence of mediums; it is the birth of a new dimension of artistic expression. Our exhibitions, which showcase a harmonious marriage of diverse mediums, invite audiences to explore the intersections of these worlds. Through each physical artwork, visitors can feel the tangible presence of the artist's hand, while digital creations challenge the limits of the possible.</p>
+                        <h1 className="title">What's behind Soma ?</h1>
+                        <p>
+                            SOMA Collective stands as a testament to the
+                            boundless power of art to transcend borders, unite
+                            diverse cultures, and connect people through a
+                            shared creative experience. Our collective mission
+                            is to bridge the realms of the physical and the
+                            digital, weaving together a tapestry of artistic
+                            expression that blurs traditional boundaries. Our
+                            passion lies in creating exhibitions worldwide that
+                            blend physical and digital artworks, crafting truly
+                            unique events that resonate across the globe.
+                        </p>
+                        <p>
+                            In a world where the boundaries between the
+                            tangible and the virtual continue to blur, SOMA
+                            Collective embraces this transformation as an
+                            opportunity for exploration. We believe that the
+                            fusion of the physical and the digital is not a
+                            mere convergence of mediums; it is the birth of a
+                            new dimension of artistic expression. Our
+                            exhibitions, which showcase a harmonious marriage
+                            of diverse mediums, invite audiences to explore the
+                            intersections of these worlds. Through each
+                            physical artwork, visitors can feel the tangible
+                            presence of the artist's hand, while digital
+                            creations challenge the limits of the possible.
+                        </p>
                     </div>
                     <div className="text-column">
-                        <p>As we embark on this global journey, we aim to offer audiences a unique experience that transcends geographic borders. By presenting our exhibitions worldwide, we enable people from diverse cultures to immerse themselves in the profound beauty and thought-provoking nature of our collective's creations. We believe that art is a universal language, and through our exhibitions, we seek to foster a global dialogue, encouraging the exchange of ideas and perspectives.</p>
-                        <p>With every physical and digital artwork, we share, we aim to connect people on a deeper level, to engage in a global conversation about the evolving role of art in the 21st century. Our desire is to break down barriers, to unite people across continents and backgrounds, and to inspire a new generation of artists and art enthusiasts who understand that art has the power to transcend boundaries and create a more interconnected world.</p>
-                        <p>As SOMA Collective continues to push the boundaries of what is possible in the intersection of physical and digital art, we invite you to join us on this global artistic journey. Together, we can build a world where creativity knows no bounds, and where art serves as a bridge that unites us all.</p>
+                        <p>
+                            As we embark on this global journey, we aim to
+                            offer audiences a unique experience that transcends
+                            geographic borders. By presenting our exhibitions
+                            worldwide, we enable people from diverse cultures
+                            to immerse themselves in the profound beauty and
+                            thought-provoking nature of our collective's
+                            creations. We believe that art is a universal
+                            language, and through our exhibitions, we seek to
+                            foster a global dialogue, encouraging the exchange
+                            of ideas and perspectives.
+                        </p>
+                        <p>
+                            With every physical and digital artwork, we share,
+                            we aim to connect people on a deeper level, to
+                            engage in a global conversation about the evolving
+                            role of art in the 21st century. Our desire is to
+                            break down barriers, to unite people across
+                            continents and backgrounds, and to inspire a new
+                            generation of artists and art enthusiasts who
+                            understand that art has the power to transcend
+                            boundaries and create a more interconnected world.
+                        </p>
+                        <p>
+                            As SOMA Collective continues to push the boundaries
+                            of what is possible in the intersection of physical
+                            and digital art, we invite you to join us on this
+                            global artistic journey. Together, we can build a
+                            world where creativity knows no bounds, and where
+                            art serves as a bridge that unites us all.
+                        </p>
                     </div>
                 </div>
             </div>
             <div className="image-container">
-                <div className="card" onClick={handleLouisDazyImgClick}>
-                    <img src={evaImgLouisDazy} alt="Eva" className="eva-image" />
-                    <div className="card-back">
-                        <p>'Eva' by Louis Dazy</p>
+                {presentationImages.map((img, index) => (
+                    <div
+                        className="card"
+                        key={index}
+                        onClick={() => navigate(img.route)}
+                        style={{
+                            '--image-url': `url(${img.src})`,
+                        } as CustomCSSProperties} // Type explicite ici
+                    >
+                        <img
+                            src={img.src}
+                            alt={img.title}
+                            className="eva-image"
+                        />
+                        <div className="card-back">
+                            <p>{`'${img.title}' by ${img.artist}`}</p>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
-    )
+    );
 };
 
 export default Home;
--- src/pages/Polygon/PolygonTab.tsx
+++ src/pages/Polygon/PolygonTab.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { SlSocialInstagram } from "react-icons/sl";
+import { FaXTwitter } from "react-icons/fa6";
 
 const PolygonTab = () => (
     <div className="container">
-        <h1>Hello polygon tab</h1>
+        <h1 className="title">Polygon1993</h1>
+        <p>
+            <a href="https://www.instagram.com/polygon1993/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <SlSocialInstagram />
+            </a>
+            <a href="https://x.com/Polygon1993?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <FaXTwitter />
+            </a>
+        </p>
+        <p className="polygonpresentation">
+            When imperfection acts as a diving force for art and analog technology stands alongside digital evolution, the past, present, and future are one ! 
+            POLYGON1993 brings us into a glitch universe that represents his daily life: a distorted world where reality and imagination mingle.
+            <br></br>
+            POLYGON1993 is a visual artist, film director and media scenographer who has made a name for himself in the world of Glitch Art. His distinct style involves using old circuit bent video gear from the 80s and 90s to create glitchy distorted lines that come together to form portraits. His art is both nostalgic and innovative, blending old technology with creative ideas to create something truly original. His clients include prestigious artists and breands such as A$AP Rocky, Tame Impala, Charli XCX, Pantone, Sony Music, Warner Music and more.
+            <br></br>
+            POLYGON1993's work has been exhibited all arount the world in many countries such as the USA, Japan, France, UK, Portugal, Italy, South Korea...and has garnered attention from art enthusiasts and collectors arount the world. He continues to explore the possibilites of glitch art and circuit-bending pushing the boundaries of what possible with his medium.
+        </p>
     </div>
 );
 
